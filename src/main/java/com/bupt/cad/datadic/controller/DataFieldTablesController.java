4f394119f933f9d313a68305427c1ee0ce83818e
yuxie <13061104801>
2021-06-19 16:59:46 +0800

    add4

--- src/main/java/com/bupt/cad/datadic/controller/DataFieldTablesController.java
+++ src/main/java/com/bupt/cad/datadic/controller/DataFieldTablesController.java
@@ -1,6 +1,7 @@
 package com.bupt.cad.datadic.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
